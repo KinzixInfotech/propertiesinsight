fb15ccb815c030a0ef526cfc19fe6af851691912
gigxmanshajami <mansajami2020@gmail.com>
2026-01-02 10:10:00 +0530

    updated

--- src/app/hare-krishna-township/page.js
+++ src/app/hare-krishna-township/page.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Script from "next/script";
 import HareKrishnaLanding from "./content";
 
 export const metadata = {
@@ -36,31 +35,5 @@ export const metadata = {
 };
 
 export default function Page() {
-    return (
-        <>
-            {/* Google Tag Manager */}
-            <Script
-                id="gtm-script"
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5222WH77');`,
-                }}
-            />
-            {/* Google Tag Manager (noscript) */}
-            <noscript>
-                <iframe
-                    src="https://www.googletagmanager.com/ns.html?id=GTM-5222WH77"
-                    height="0"
-                    width="0"
-                    style={{ display: 'none', visibility: 'hidden' }}
-                />
-            </noscript>
-            {/* End Google Tag Manager (noscript) */}
-            <HareKrishnaLanding />
-        </>
-    );
+    return <HareKrishnaLanding />;
 }
